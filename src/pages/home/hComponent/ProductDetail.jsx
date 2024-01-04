c4cae27b59bfd5fc65a52c8859e1d3e723af94a7
mk-saadi <mksaadi820@gmail.com>
2024-01-05 03:44:30 +0600

    rating star

--- src/pages/home/hComponent/ProductDetail.jsx
+++ src/pages/home/hComponent/ProductDetail.jsx
@@ -2,9 +2,15 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import ProductSwiper from "./ProductSwiper";
+import { StarHalf, StarIcon } from "lucide-react";
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
 
 const ProductDetail = () => {
 	const [product, setProduct] = useState([]);
+	console.log("product: ", product);
 	const { id } = useParams();
 
 	useEffect(() => {
@@ -32,11 +38,48 @@ const ProductDetail = () => {
 				</div>
 
 				<div>
-					<p className="text-gray-700">{product.title}</p>
+					<div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+						<h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+							{product.title}
+						</h1>
+					</div>
+
+					<div className="mt-4 lg:row-span-3 lg:mt-0">
+						<h2 className="sr-only">Product information</h2>
+						<p className="text-3xl tracking-tight text-gray-900">
+							{product.price}
+						</p>
+
+						{/* Reviews */}
+						<div className="flex rating-stars text-amber-500">
+							<RatingStars rating={product.rating} />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
+};
+
+const RatingStars = ({ rating }) => {
+	const renderStars = () => {
+		const fullStars = Math.floor(rating);
+		const hasHalfStar = rating % 1 !== 0;
+
+		const stars = [];
+
+		for (let i = 0; i < fullStars; i++) {
+			stars.push(<StarIcon key={i} />);
+		}
+
+		if (hasHalfStar) {
+			stars.push(<StarHalf key="half" />);
+		}
+
+		return stars;
+	};
+
+	return <div className="flex items-center">{renderStars()}</div>;
 };
 
 export default ProductDetail;
